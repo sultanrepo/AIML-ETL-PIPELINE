8194c656a87d13dd245d38ba4271255a70983756
Sultan Ashraf <sultanashraf.repo@gmail.com>
2026-02-13 15:42:12 +0530

    Add comment

--- etl/extract.py
+++ etl/extract.py
@@ -10,3 +10,4 @@ def extract_rows(cursor, table, last_ts):
     rows = cursor.fetchall()
     columns = [col[0] for col in cursor.description]
     return rows, columns
+# Extraction of data from the source database based on the last updated timestamp.
